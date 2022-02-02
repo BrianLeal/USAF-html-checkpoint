66a619fd704bc72996aa536c5f830b6bf4d76bab
Brian Leal <contact@brianleal.com>
2022-02-02 13:15:50 -0700

    checkpoint framework

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var imgSelect = document.getElementById("tester"); //test on button
+
+// var redirect = function(){
+//     alert('you clicked the image');
+// }
+
+imgSelect.onclick(alert('You clicked on the button'));
+// imgSelect.addEventListener('click', redirect); 
